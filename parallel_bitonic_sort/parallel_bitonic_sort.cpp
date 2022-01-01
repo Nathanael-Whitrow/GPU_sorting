6933370dd8b29b0fc4a22397f4e0b0497b2de782
n8 <nathanael.whitrow@gmail.com>
2022-01-01 13:56:30 +1030

    Refactor to Modern c++ Style
    Code should now look like modern c++ where possible and run on
    linux, windows and apple platforms.
    However at time of writing this is only tested on a linux system.

--- parallel_bitonic_sort/parallel_bitonic_sort.cpp
+++ parallel_bitonic_sort/parallel_bitonic_sort.cpp
@@ -4,13 +4,11 @@
  * for PDC 2019, assignment 3
 */
 
-
+// LIMITATION/BUG: The size of this may cause "malloc" to fail on some systems.
 #define ITEMS 1024*1024
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <Windows.h>
+#include <iostream>
+#include <ctime>
 
 
 #ifdef __APPLE__
@@ -22,49 +20,31 @@
 #define MAX_SOURCE_SIZE (0x100000)
 
 
-#include <Windows.h>
-double get_wall_time()
-{
-	LARGE_INTEGER time, freq;
-	if (!QueryPerformanceFrequency(&freq))
-	{
-		//  Handle error
-		return 0;
-	}
-	if (!QueryPerformanceCounter(&time))
-	{
-		//  Handle error
-		return 0;
-	}
-	return (double)time.QuadPart / freq.QuadPart;
-}
-
-
-
-
 int main(void)
 {
 	// timing vars
-	double start_time, end_time, total_time;
+	std::time_t start_time, end_time, total_time;
 
-	/* create a list of 32bit unsigned ints. */
+	std::time(&start_time);
+	// create a list of 32bit unsigned ints.
 	int i;
 	const int LIST_SIZE = ITEMS;
 	cl_uint *INPUT = (cl_uint*)malloc(sizeof(cl_uint)*LIST_SIZE);
 	for (i = 0; i < LIST_SIZE; ++i)
-		INPUT[i] = rand();
+		INPUT[i] = rand(); // on g++ for c++ 20 RAND_MAX = 2,147,483,647
 
-	/*
-		Creates a file pointer, string pointer and size var.
-		attempts to open kernel.cl
-		allocates memory for the source_str
-		reads the string and puts it into memory
-		closes file
-	*/
-	
-	start_time = get_wall_time();
+	std::time(&end_time);
+	total_time = end_time - start_time;
+	std::cout << "List creation time: " <<  total_time << " seconds." << std::endl;
 
 
+
+	// Creates a file pointer, string pointer and size var.
+	// attempts to open kernel.cl
+	// allocates memory for the source_str
+	// reads the string and puts it into memory
+	// closes file
+	std::time(&start_time);
 	FILE *fp;
 	char *source_str;
 	size_t source_size;
@@ -72,18 +52,19 @@ int main(void)
 	fp = fopen("kernel.cl", "r");
 	if (!fp)
 	{
-		fprintf(stderr, "Failed to load kernel.\n");
+		std::cerr << "Failed to load kernel." << std::endl;
 		exit(1);
 	}
 	source_str = (char*)malloc(MAX_SOURCE_SIZE);
 	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
 	fclose(fp);
 	
-	end_time = get_wall_time();
+	std::time(&end_time);
 	total_time = end_time - start_time;
-	printf("done. List creation time: %f\n", total_time);
+	std::cout << "Kernel compile time: " <<  total_time << " seconds." << std::endl;
 
-	start_time = get_wall_time();
+	// OpenCL setup
+	std::time(&start_time);
 
 	// OpenCl data structures
 	cl_platform_id platform_id = NULL;
@@ -92,22 +73,21 @@ int main(void)
 	cl_uint err_num_platforms;
 	cl_int err;
 
-	// Gets the platform ID and places it in platform id. Platform ID is the platform, i.e. nVidia, Intel, etc.
+	// Gets the platform ID and places it in platform_id. Platform ID is the platform, i.e. nVidia, Intel, etc.
 	err = clGetPlatformIDs(1, &platform_id, &err_num_platforms);
 
-	// Given we know the platform is nVidia, get a list of the available GPU's
+	// Given we know the platform is a GPU, get a list of the available GPU's
 	err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &err_num_devices);
 
 	// Makes a context with the GPU in it
 	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &err);
 
 	// Sets up a command queue for the context
-	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, NULL, &err);
+	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &err);
 
 	// creates buffers to hold data that can be handed back and forth from GPU memory and host-process memory
 	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, LIST_SIZE * sizeof(cl_uint), NULL, &err);
-	//cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(cl_uint), NULL, &err);
-
+	
 	// enqueuing a write buffer copies that memory from host process to GPU
 	err = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(cl_uint), INPUT, 0, NULL, NULL);
 
@@ -129,7 +109,7 @@ int main(void)
 
 	size_t local_item_size;
 	err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &local_item_size, NULL);
-	printf("local work group size: %d\n", local_item_size);
+	std::cout << "local work group size: " << local_item_size << std::endl;
 
 	cl_event event;
 
@@ -139,23 +119,16 @@ int main(void)
 	cl_uint direction = 1;
 	
 	err = clSetKernelArg(kernel, 3, sizeof(cl_uint), (void *)&direction);
-
-	/*
-	for (i = 0; i < LIST_SIZE; ++i)
-		printf("%d\n", INPUT[i]);
-	printf("now do kernel stuff\n");
-	*/
-	
 	
 	// figure out how many stages are needed
 	for (i = LIST_SIZE; i > 1; i >>= 1)
 		total_stages++;
 	
-	end_time = get_wall_time();
+	std::time(&end_time);
 	total_time = end_time - start_time;
-	printf("done. setup time: %f\n", total_time);
+	std::cout << "Setup time: " <<  total_time << " seconds." << std::endl;
 	
-	start_time = get_wall_time();
+	std::time(&start_time);
 
 	for (current_stage = 0; current_stage < total_stages; ++current_stage)
 	{
@@ -165,39 +138,26 @@ int main(void)
 		for (pass = 0; pass < current_stage + 1; ++pass)
 		{
 			err = clSetKernelArg(kernel, 2, sizeof(cl_uint), (void *) &pass);
-
-			//printf("stage = %d, pass = %d\n", current_stage, pass);
-
 			err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, &event);
 		}
 	}
 	
-	end_time = get_wall_time();
-
+	std::time(&end_time);
 	total_time = end_time - start_time;
-	printf("done. sorting time: %f\n", total_time);
-	/*
-	clWaitForEvents(1, &event);
-	cl_ulong time_start, time_end;
-	double total_time;
-	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(time_start), &time_start, NULL);
-	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(time_end), &time_end, NULL);
-	total_time = time_end - time_start;
-	*/
+	std::cout << "Sorting time: " <<  total_time << " seconds." << std::endl;
 
 	err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &local_item_size, NULL);
-	printf("max item size: %d\n", global_item_size);
-	
+	std::cout << "max item size: " << global_item_size << std::endl;
 
 	// creates space for the output in the host memory and copies the data from GPU memory to host memory
 	err = clEnqueueReadBuffer(command_queue, a_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(cl_uint), INPUT, 0, NULL, NULL);
 
 	// print out the results
-	printf("checking...\n");
+	std::cout << "checking..." << std::endl;
 	for (i = 0; i < LIST_SIZE - 1; ++i)
 		if(INPUT[i] > INPUT[i+1])
-			printf("BAD SORT!\n");
-	printf("done checking.\n");
+			std::cout << "BAD SORT!" << std::endl;
+	std::cout << "done checking." << std::endl;
 
 	// clean up
 	err = clFlush(command_queue);
